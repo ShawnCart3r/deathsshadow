8a5808f66edfe663f5dffcfc57b4cd337dd9b571
Shawn Carter <53487411+ShawnCart3r@users.noreply.github.com>
2021-03-28 23:08:53 -0400

    Add files via upload

--- java.js
+++ java.js
@@ -1,29 +1,23 @@
 
 //power
 let power = 13;
-document.getElementById('lifeTotal').onkeyup = function(){
+let toughness = 13;
+document.getElementById('lifePower').onkeyup = function(){
   if(this.value.length == 0) {
     document.getElementById('power').innerHTML = '';
     return;
   }
-  let number = parseInt(this.value);
-  if(isNaN(number)) return;
-  document.getElementById('power').innerHTML = power - number;
+  let setPower = parseInt(this.value);
+  if(isNaN(setPower)) return;
+  document.getElementById('power').innerHTML = power - setPower;
+  let setToughness = parseInt(this.value);
+  if(isNaN(setToughness)) return;
+  document.getElementById('toughness').innerHTML = toughness - setToughness;
 };
-document.getElementById('lifeTotal').onkeyup();
+document.getElementById('lifePower').onkeyup();
 
 //toughness
-let toughness = 13;
-document.getElementById('lifeTotal').onkeyup = function(){
-  if(this.value.length == 0) {
-    document.getElementById('toughness').innerHTML = '';
-    return;
-  }
-  let number = parseInt(this.value);
-  if(isNaN(number)) return;
-  document.getElementById('toughness').innerHTML = toughness - number;
-};
-document.getElementById('lifeTotal').onkeyup();
+
 
 
 
